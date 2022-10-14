8ca63c5a60507630827f0b64528c58e86b481794
tnlanh99 <tnlanh99@gmail.com>
2022-10-15 00:20:29 +0700

    Install Chakra UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import {
+  Box,
+  Center,
+  ChakraProvider,
+  Heading,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+
 const App = () => {
-  return <h1>Lanh&apos;s Blog</h1>;
+  return (
+    <ChakraProvider>
+      <Box w="100vw" h="100vh">
+        <Center h="100%">
+          <VStack>
+            <Heading>Lanh&apos;s Blog</Heading>
+            <Text>🚧 🚧 Building 🚧 🚧</Text>
+          </VStack>
+        </Center>
+      </Box>
+    </ChakraProvider>
+  );
 };
 
 export default App;
